706a19096eb742d8cfc78ce687a2cffaa251d9b4
Pragyashree jaiswal <pragyashreejaiswal@gmail.com>
2023-06-15 19:55:25 +0530

    second commit

--- app.js
+++ app.js
@@ -36,6 +36,6 @@ app.post("/", function(req,res){
 
 })
 
-app.listen(3000, function(){
-    console.log("Setver is running on port 3000");
-})
+app.listen(process.env.PORT || 3000, function(){
+    console.log("server is running on port 3000");
+  });
